20e1724f0b4c3898caad1d7d473fd76ce5b56c75
Boniface Maina <karisbm@gmail.com>
2020-04-08 15:05:52 +0300

    (feature) calculate currentlyInfected and infectionsByRequestedTime

--- src/estimator.js
+++ src/estimator.js
@@ -1,23 +1,28 @@
 const covid19ImpactEstimator = (data) => {
-  const input = data;
-  let currentlyInfected;
-  if (input.periodType === 'weeks') {
-    input.timeToElapse *= 7;
-  }
-  if (input.periodType === 'months') {
-    input.timeToElapse *= 30;
-  }
-  return {
-    data: input,
-    impact: {
-      currentlyInfected: input.reportedCases * 10,
-      infectionsByRequestedTime: Math.floor(currentlyInfected * (2 ** (input.timeToElapse / 3)))
-    },
-    severeImpact: {
-      currentlyInfected: input.reportedCases * 50,
-      infectionsByRequestedTime: Math.floor(currentlyInfected * (2 ** (input.timeToElapse / 3)))
+    let currentlyInfected;
+    
+    //   estimate in days
+    const estimateInDays = (data) =>{
+        const input = data;
+        return {
+            data: input,
+            impact: {
+            currentlyInfected: input.reportedCases * 10,
+            infectionsByRequestedTime: Math.floor(currentlyInfected * (2 ** (input.timeToElapse / 3)))
+            },
+            severeImpact: {
+            currentlyInfected: input.reportedCases * 50,
+            infectionsByRequestedTime: Math.floor(currentlyInfected * (2 ** (input.timeToElapse / 3)))
+            }
+        };
     }
-  };
+//   if (input.periodType === 'weeks') {
+//     input.timeToElapse *= 7;
+//   }
+//   if (input.periodType === 'months') {
+//     input.timeToElapse *= 30;
+//   }
+  
 };
 
 export default covid19ImpactEstimator;
